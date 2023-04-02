6987559f6a7a921de1d667bc356806a307531bd0
Tien_Phan <tien0202029@gmail.com>
2023-04-02 17:12:01 +0700

    Update files

--- te.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-df = pd.read_csv("InfomationRestaurants.csv", sep=",", header=None, 
-                 names=["name", "address", "phone", "timeOpen", "ratingAverage", "priceRange", "details"])
-print(df)
--- test.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df= pd.read_csv('E:\Crawl Tripadvisor\restaurantInfo.csv')
-df
