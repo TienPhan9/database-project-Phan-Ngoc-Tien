d9c2b9d0a53585588a866bb0c5a6b14a56557d6b
Tien_Phan <tien0202029@gmail.com>
2023-04-26 21:44:52 +0700

    Process price

--- preprocess/price.sql
+++ preprocess/price.sql
@@ -22,7 +22,7 @@ from restaurant_copy
 --where priceRange is not null
 order by len(priceRange)
 
---xong phần outlier
+-->xong phần outlier
 update priceCook
 set minimum = right(minimum,5), maximum = right(maximum, 6)
 where len(minimum)=10 or len(maximum) =10
@@ -47,7 +47,6 @@ where len(maximum) - len(minimum) =5
 update priceCook
 set minimum = minimum + '000'
 where len(maximum) - len(minimum) =4
-
 --with distance = 3
 /*in this case, there is a record that have minimum = 3 and maximum = 1000, so fix particularly
 with each record*/
@@ -58,6 +57,250 @@ where id = 2061
 update priceCook
 set minimum = minimum + '00000' , maximum = maximum + '0000'
 where id = 1288
+
+-------------
+--handle data with values are too small
+select *, len(maximum) from priceCook
+--where len(minimum) =1 and len(maximum)=1
+order by len(minimum), len(maximum)
+
+--with minimum has len =1 and maximum has len =1
+update priceCook
+set minimum = minimum + '0000', maximum = maximum+ '00000'
+where len(minimum) =1 and len(maximum)=1
+
+--with minimum has len =1 and maximum has len =2
+update priceCook
+set minimum = minimum + '0000', maximum = maximum+ '0000'
+where len(minimum) =1 and len(maximum)=2
+
+--with minimum has len =1 and maximum has len =3
+update priceCook
+set minimum = minimum + '0000', maximum = maximum+ '000'
+where len(minimum) =1 and len(maximum)=3
+
+--with minimum has len =2 and maximum has len =2
+update priceCook
+set minimum = minimum + '000', maximum = maximum+ '0000'
+where len(minimum) =2 and len(maximum)=2
+
+--with minimum has len =2 and maximum has len =3
+update priceCook
+set minimum = minimum + '000', maximum = maximum+ '000'
+where len(minimum) =2 and len(maximum)=3
+
+--with minimum has len =3 and maximum has len =3
+
+update priceCook
+set minimum = minimum + '000', maximum = maximum+ '000'
+where len(minimum) =3 and len(maximum)=3
+
+--with minimum has len =3 and maximum has len =4
+select *, len(maximum) from priceCook
+--where len(minimum) =3 and len(maximum)=4
+order by len(minimum), len(maximum)
+
+update priceCook
+set minimum = minimum + '00', maximum = maximum+ '00'
+where len(minimum) =3 and len(maximum)=4
+
+--with minimum has len =3 and maximum has len =5
+update priceCook
+set minimum = minimum + '000', maximum = maximum+ '0'
+where len(minimum) =3 and len(maximum)=5
+
+--with minimum has len =4 and maximum has len =4
+update priceCook
+set minimum = minimum + '00', maximum = maximum+ '00'
+where len(minimum) =4 and len(maximum)=4
+
+--finish dirty price, turning to null price
+--NOTE
+--with details like N'%Cà phê%Kiểu Á%Kiểu Việt%Đồ uống%'
+
+update priceCook
+set minimum = (select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where details like N'%Cà phê%Kiểu Á%Kiểu Việt%Đồ uống%' and priceRange is not null)
+where details like N'%Cà phê%Kiểu Á%Kiểu Việt%Đồ uống%' and priceRange is null
+
+--with details like N'Kiểu Việt'
+update priceCook
+set minimum = (select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where details like N'Kiểu Việt' and priceRange is not null)
+where details like N'Kiểu Việt' and priceRange is null
+
+/*where (details like N'%Kiểu Á%Kiểu Việt%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%'*/
+
+update priceCook
+set minimum = (select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Á%Kiểu Việt%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and minimum is not null)
+where (details like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and minimum is null
+
+/*with details like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%'*/
+update priceCook
+ set minimum = (select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and minimum is not null)
+where (details like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and minimum is null
+--with Kiểu Mỹ
+update priceCook
+set minimum = (select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+minimum is not null)
+where (details like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+minimum is null
+
+--with Kiểu Nhật
+update priceCook
+set minimum = (select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+minimum is not null)
+where (details like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+minimum is null
+
+--with 
+update priceCook
+set minimum = (select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Âu%' and details not like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+minimum is not null)
+where (details like N'%Kiểu Âu%' and details not like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+minimum is null
+
+--the rest will be added as rest values
+update priceCook
+set minimum = (
+select cast(avg(cast(minimum as bigint)) as varchar) from priceCook
+where minimum is not null)
+where minimum is null
+
+-->finish minimum
+
+--maximum
+--with details like N'%Cà phê%Kiểu Á%Kiểu Việt%Đồ uống%'
+update priceCook
+set maximum = (select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where details like N'%Cà phê%Kiểu Á%Kiểu Việt%Đồ uống%' and priceRange is null)
+where details like N'%Cà phê%Kiểu Á%Kiểu Việt%Đồ uống%' and priceRange is null
+
+--with details like N'Kiểu Việt'
+update priceCook
+set maximum = (select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where details like N'Kiểu Việt' and priceRange is not null)
+where details like N'Kiểu Việt' and priceRange is null
+
+/*where (details like N'%Kiểu Á%Kiểu Việt%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%'*/
+
+update priceCook
+set maximum = (select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Á%Kiểu Việt%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and maximum is not null)
+where (details like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and maximum is null
+
+/*with details like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%'*/
+update priceCook
+ set maximum = (select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and maximum is not null)
+where (details like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%')
+and maximum is null
+--with Kiểu Mỹ
+update priceCook
+set maximum = (select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+maximum is not null)
+where (details like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+maximum is null
+
+--with Kiểu Nhật
+update priceCook
+set maximum = (select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+maximum is not null)
+where (details like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+maximum is null
+
+--with 
+update priceCook
+set maximum = (select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where (details like N'%Kiểu Âu%' and details not like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+maximum is not null)
+where (details like N'%Kiểu Âu%' and details not like N'%Kiểu Nhật%' and details not like N'%Kiểu Mỹ%' and 
+details not like N'%Kiểu Á%' and details not like N'%Kiểu Á%Kiểu Việt%'
+and details not like N'%Kiểu Việt%Kiểu Á%'
+ and details not like N'%Cà phê%Kiểu Á%Kiểu Việt%') and 
+maximum is null
+
+--the rest will be added as rest values
+update priceCook
+set maximum = (
+select cast(avg(cast(maximum as bigint)) as varchar) from priceCook
+where maximum is not null)
+where maximum is null
+
+-->finish maximum
+select * from priceCook
+where len(minimum) > len(maximum) --check again
+
+-----------------------------
+
+
+
+
 
 
 
