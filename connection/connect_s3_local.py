f9c8ccb1cf4fa157782c7f8d35e67e29b3455f54
Tien_Phan <tien0202029@gmail.com>
2023-04-29 08:03:29 +0700

    Load fixed file S3

--- connection/connect_s3_local.py
+++ connection/connect_s3_local.py
@@ -1,10 +1,10 @@
 import boto
 
-s3 = boto.connect_s3('AKIATLW3FXJQBD34SWHJ', 'Wt8UaRhUUCgiDA+iKJAJ3fU5y5UEKUJHtIYo20aK')
+s3 = boto.connect_s3('Private', 'Private')
 
 bucket_name = 'data-aws-9'
 bucket = s3.get_bucket(bucket_name)
 
-file_key = 'fixed_restaurants_s3.csv'
+file_key = 'merged_restaurants_s3.csv'
 key = bucket.get_key(file_key)
 key.get_contents_to_filename('E:/Crawl Tripadvisor/data/s3_restaurant.csv')
